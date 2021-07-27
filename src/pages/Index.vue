685a6ad7804a842934a26751b8b00749cbe63eb2
Sébastien Prud'homme <sebastien.prudhomme@gmail.com>
2021-07-27 09:33:25 +0200

    WIP

---
+++ src/pages/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page class="flex flex-center">
+    {{ version }}
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { ipcRenderer } from 'electron'
+
+export default defineComponent({
+  name: 'PageIndex',
+  data () {
+    return {
+      version: ''
+    }
+  }
+  // mounted: function () {
+  //   ipcRenderer.send('app_version')
+
+  //   ipcRenderer.on('app_version', (event, arg) => {
+  //     ipcRenderer.removeAllListeners('app_version')
+  //     this.version = 'Version ' + arg.version
+  //   })
+  // }
+})
+</script>
